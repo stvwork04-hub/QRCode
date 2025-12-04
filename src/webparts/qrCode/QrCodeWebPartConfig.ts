ac5195d141e5ab073e20a18aaba5aa5d75acb7df
stvwork04-hub <stvwork04@gmail.com>
2025-12-04 16:06:07 +0300

    Changed the PA http URL to the latest V3 flow.

--- src/webparts/qrCode/QrCodeWebPartConfig.ts
+++ src/webparts/qrCode/QrCodeWebPartConfig.ts
@@ -7,5 +7,5 @@ export interface IQrCodeWebPartConfig {
 export const QrCodeWebPartConfig: IQrCodeWebPartConfig = {
   siteUrl: 'https://tecq8.sharepoint.com/sites/DigitalBusinessCard',
   listName: 'DigitalBusinessCards',
-  powerAutomateUrl: 'https://0d8a5cb67bd747f6b7d0905b392268.d1.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/e015ead33fae4e3fb92520c44abcd344/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=iwE0bpVAv5yngCMksvxC9BGS5kyC6frA8tuIH5Eh3JE'
+  powerAutomateUrl: 'https://0d8a5cb67bd747f6b7d0905b392268.d1.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/b89862b78ac4460c92927cde489001f7/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=7E7z2lqp6CU1uGgh9eOcZ_74K-852GZQc4lqvyo0Z2k'
 };
